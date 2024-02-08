7c62e809f1cb02fd4b7c060ef752ce0e5b399a35
pbuitron <pbuitron@users.noreply.github.com>
2024-02-07 22:44:14 -0500

    90%

--- src/pages/Como-Aplicar.jsx
+++ src/pages/Como-Aplicar.jsx
@@ -7,7 +7,7 @@ const ComoAplicar = () => {
     <>
       <header>
         <div
-          className="p-5 text-center bg-image container"
+          className="p-5 text-center bg-image "
           style={{
             backgroundImage:
               "url('https://sylwrap.com/wp-content/uploads/2021/04/Contact-Header-SylWrap.jpg')",
