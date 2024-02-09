127f59ac8d063f5e9b9050537a644f959a270bfa
pbuitron <pbuitron@users.noreply.github.com>
2024-02-09 16:36:06 -0500

    FInalizado para modificaciones postriores

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 /*import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
 */import './Footer.css'
-import { Link } from 'react-router-dom';
+
 export default function Footer() {
   return (
     <footer className="bg-body-tertiary text-center">
@@ -18,119 +18,7 @@ export default function Footer() {
 </footer>
 
 
-      /*<MDBFooter bgColor='light' className='formato text-center text-lg-start text-muted'>
-      <svg
-    preserveAspectRatio="none"
-    viewBox="0 0 1200 120"
-    xmlns="http://www.w3.org/2000/svg"
-    style={{ fill: '#216a9e', width: '100%', height: 65, transform: 'scaleX(-1)' }}
-  >
-    <path d="M1200 120L0 16.48V0h1200v120z" />
-  </svg>
-      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-        <div className='me-5 d-none d-lg-block'>
-          <span>Encuentranos en nuestras redes sociales:</span>
-        </div>
-
-        <div>
-          <a href='www.facebook.com' className='me-4 text-reset'>
-            <MDBIcon fab icon="facebook-f" />
-            <MDBIcon className='ms-1' icon='camera' size='sm' />
-          </a>
-          <a href='www.twitter.com' className='me-4 text-reset'>
-            <MDBIcon fab icon="twitter" />
-          </a>
-          <a href='www.google.com' className='me-4 text-reset'>
-            <MDBIcon fab icon="google" />
-          </a>
-          <a href='instagram' className='me-4 text-reset'>
-            <MDBIcon fab icon="instagram" />
-          </a>
-          <a href='www.linkedin.com' className='me-4 text-reset'>
-            <MDBIcon fab icon="linkedin" />
-          </a>
-          
-        </div>
-      </section>
-
-      <section className=''>
-        <MDBContainer className='text-center text-md-start mt-5'>
-          <MDBRow className='mt-3'>
-            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon icon="gem" className="me-3" />
-                SEALPRO
-              </h6>
-              
-            </MDBCol>
-
-            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Productos</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Reparación de Fugas
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Reparación de Corrosión y Refuerzos
-                </a>
-              </p>
-              
-            </MDBCol>
-
-            <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Pricing
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Settings
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Help
-                </a>
-              </p>
-            </MDBCol>
-
-            <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-              <p>
-                <MDBIcon icon="home" className="me-2" />
-                Lima, Perú
-              </p>
-              <p>
-                <MDBIcon icon="envelope" className="me-3" />
-                ventas@sealpro.site
-              </p>
-              <p>
-                <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
-              </p>
-              <p>
-                <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
-              </p>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </section>
-
-      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-        © 2024 Copyright:
-        <Link className='text-reset fw-bold' to='https://mdbootstrap.com/'>
-          Paul Buitron
-        </Link>
-      </div>
-    </MDBFooter>*/
+     
   );
 }
 
